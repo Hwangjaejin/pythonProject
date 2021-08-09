08cda4a28d58bdc919382852e2db1aad86ad0853
jaejin <jaejin94@ajou.ac.kr>
2021-08-10 01:00:07 +0900

    크롤링 방법 주석추가

--- hello.py
+++ hello.py
@@ -1,12 +1,12 @@
-import requests
-from bs4 import BeautifulSoup
+import requests # 크롤링 요청할 때 사용.(API 사용시 필요)
+from bs4 import BeautifulSoup # 긁어 온 데이터를 잘 뽑아내기 위한 라이브러리
 
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
 
 soup = BeautifulSoup(data.text, 'html.parser')
 
-trs = soup.select('#old_content > table > tbody > tr')
+trs = soup.select('#old_content > table > tbody > tr') # 웹에서 필요한 정보 우클릭 > 검사 > 해당영역 우클릭 > copy > copy selector
 
 for tr in trs:
     a_tag = tr.select_one('td.title > div > a')
