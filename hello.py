15d78c336d72d067eac6d6869bd65b3373718828
jaejin <jaejin94@ajou.ac.kr>
2021-08-10 10:12:44 +0900

    주석추가

--- hello.py
+++ hello.py
@@ -1,10 +1,14 @@
 import requests # 크롤링 요청할 때 사용.(API 사용시 필요)
 from bs4 import BeautifulSoup # 긁어 온 데이터를 잘 뽑아내기 위한 라이브러리
 
+from pymongo import MongoClient
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
 headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
 data = requests.get('https://movie.naver.com/movie/sdb/rank/rmovie.nhn?sel=pnt&date=20200303',headers=headers)
 
-soup = BeautifulSoup(data.text, 'html.parser')
+soup = BeautifulSoup(data.text, 'html.parser') # soup : 해당페이지 html전체
 
 trs = soup.select('#old_content > table > tbody > tr') # 웹에서 필요한 정보 우클릭 > 검사 > 해당영역 우클릭 > copy > copy selector
 
@@ -14,4 +18,10 @@ for tr in trs:
         rank = tr.select_one('td:nth-child(1) > img')['alt']
         title = a_tag.text
         star = tr.select_one('td.point').text
-        print(rank, title, star)
+
+        doc = {
+            'rank':rank,
+            'title':title,
+            'start':star
+        }
+        db.movies.insert_one(doc)
